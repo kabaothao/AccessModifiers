c3ee3078ba4b8a0c238811bdabb48de0bcbbb1c2
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-03 17:38:04 -0600

    updated codes

---
+++ Amazon/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon
+{   
+
+    public class RateCalculator
+    {
+        public int Claculate(Customer customer)
+        {
+            return 0;
+        }
+    }
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        public void Promote()
+        {   
+
+            var calculator = new RateCalculator();
+            var rating = calculator.Claculate(this);
+
+            //var rating = CalculateRating(excludeOrders: true);
+            //if (rating == 0)
+            //    Console.WriteLine("Promoted to Level 1");
+            //else
+            //    Console.WriteLine("Promoted to Level 2");
+
+            Console.WriteLine("Promote logic changed.");
+        }
+
+
+        //protected int CalculateRating(bool excludeOrders)
+        //{
+        //    return 0;
+
+        //}
+
+
+
+    }
+}
