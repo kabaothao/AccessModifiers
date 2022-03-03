65e48110c142cce2460874d53edd7f178c8656df
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-03 09:37:42 -0600

    updated codes

--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+namespace AccessModifiers
+{
+
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            var customer = new Customer();
+            //var rating = customer.CalculateRating();
+        }
+
+
+
+
+    }
+
+
+
+
+}
